e82193f509e74f7f866898574fd81ba9008a3c92
R Lewis <roblewis1237@gmail.com>
2018-08-15 12:20:59 +0100

    making app available to external IPs

--- run_keras_server.py
+++ run_keras_server.py
@@ -84,4 +84,4 @@ if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
            "please wait until server has fully started"))
     load_model()
-    app.run()
+    app.run(host='0.0.0.0') # add host arg so it's available on external addresses
